9c2570f1987d41b28c53f77fcb4a49b7632f6a10
_Entity014 ( Narawit Inyos ) <63171588+Entity014@users.noreply.github.com>
2024-03-05 11:37:41 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,169 +1,178 @@
+//// Assign library for base box control project
 #include <Arduino.h>
-#include <pid.h>
-#include <WiFi.h>
+#include <LiquidCrystal_I2C.h>
 #include <MQTT.h>
-#include <motor.h>
-#include <NewPing.h>
-#include <SimpleKalmanFilter.h>
+#include <WiFi.h>
 
-// ? Define Values
-/* Ultrasonic */
-#define ECHO 32
-#define TRIG 5
-unsigned int distance;
-NewPing sonar(TRIG, ECHO, 200);
-SimpleKalmanFilter simpleKalmanFilter(2, 2, 0.01);
+//// Assign pin of actuator and variable
+#define Button_Pin   2
+#define Buzz 32
+#define Vol     34
+boolean sent = false;
+boolean wait = false;
+String msg;
+String pre_msg;
 
-/* Motor */
-#define MOTOR_INA 0
-#define MOTOR_INB 2
-#define MOTOR_PWM 4
-#define K_P 6
-#define K_I 0.0173
-#define K_D 0.000025
-Motor motor_controller(20000, 10, false, MOTOR_PWM, MOTOR_INA, MOTOR_INB);
-PID motor_pid((pow(2, 10) - 1) * -1, pow(2, 10) - 1, K_P, K_I, K_D);
-
-int setpoint = 0;
-
-/* Limit switch */
-#define LIM_SWITCH 33
-int pre_btn = 0;
-int state = 0;
-
-/* WIFI & MQTT*/
+//// Config topic from library
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set up LCD-I2C 16 Col 2 Row
 WiFiClient net;
 MQTTClient client;
 
+//// Config wifi path
 const char ssid[] = "@JumboPlusIoT";
 const char pass[] = "12345678";
 
-const char mqtt_broker[] = "test.mosquitto.org";
-const char mqtt_height[] = "project1.21/heights";
-const char mqtt_distance[] = "project1.21/distance";
-const char mqtt_client_id[] = "arduino_project1.21_drone";
-int MQTT_PORT = 1883;
+//// Config sever path
+const char mqtt_broker[]="test.mosquitto.org";
+const char mqtt_topic[]="project1.21/heights";
+const char mqtt_sub[]="project1.21/#";
+const char mqtt_client_id[]="arduino_project_1.21"; // must change this string to a unique value
+int MQTT_PORT=1883;
 
-// ? prototype fuction
-/* Limit switch */
-void IRAM_ATTR IO_INT_ISR();
-
-/* WIFI & MQTT*/
+//// Function Topic
 void connect();
 void messageReceived(String &topic, String &payload);
+void task_arduino(void *arg);
 
-void setup()
-{
-  /* This method have 2 parts
-      part 1 (Limit switch) : define pinmode of limit switch and attach interrupt with method call IO_INT_ISR
-      part 2 (WIFI & MQTT) : connect wifi and mqtt
-  */
-  Serial.begin(115200);
-
-  /* Limit switch */
-  pinMode(LIM_SWITCH, INPUT_PULLUP);
-  attachInterrupt(LIM_SWITCH, IO_INT_ISR, CHANGE);
-
-  /* WIFI & MQTT*/
+//// Setup command
+void setup(){
+  Serial.begin(9600);
+  pinMode(Vol,INPUT);
+  pinMode(Button_Pin,INPUT);
+	lcd.init();
+	lcd.backlight();
   WiFi.begin(ssid, pass);
   client.begin(mqtt_broker, MQTT_PORT, net);
   client.onMessage(messageReceived);
   connect();
+
+  //// Method for using multicore esp32
+  xTaskCreatePinnedToCore(
+      task_arduino,
+      "Task1",
+      8192,
+      NULL,
+      0,
+      NULL,
+      0);
 }
 
-void loop()
-{
-  /* This method have 3 parts
-    part 1 (MQTT) : loop connection between mqtt client and mqtt broker than mqtt client publish value (distance) to topic (mqtt_distance)
-    part 2 (Ultrasonic) : find distance of drone from base station and filter distance by SimpleKalmanFilter
-    part 3 (Motor) : command motor with speed tuing by PID
-      state 0 > command motor with speed tuing by PID
-      state 1 > command motor with 60% of speed
-  */
-  /* MQTT*/
+void loop(){
+  // connect client
   client.loop();
-  if (!client.connected())
-  {
+  if(!client.connected()){
     connect();
   }
 
-  /* Ultrasonic */
-  unsigned int uS = sonar.ping();
-  distance = constrain((uS / US_ROUNDTRIP_CM) - 2.8, 0, 100);
-  distance = simpleKalmanFilter.updateEstimate(distance);
-  if (setpoint == 0 && state == 1 && distance <= 1)
-  {
-    state = 0;
+  // Interface running while get high level for tuning
+  if(!sent && !wait){
+    lcd.setCursor(3,0);
+    lcd.print("PID Tuning");
+    lcd.setCursor(0,1);
+    lcd.print("Height(cm): ");
+    lcd.setCursor(12,1);
+    lcd.print(String(map(analogRead(Vol), 0, 4095, 0, 80)));
   }
-  /* Motor */
-  if (!state)
-  {
-    motor_controller.spin(motor_pid.compute(setpoint, distance));
-  }
-  else
-  {
-    motor_controller.spin(614);
-  }
-  Serial.println(String(distance) + " " + String(setpoint) + " " + String(motor_controller.getPwm()) + " " + String(state));
 
-  /* MQTT*/
-  client.publish(mqtt_distance, String(distance));
+  // Interface running while tuning and showwing tuning point and current heights msg for tuning from base box
+  if(wait){
+    lcd.setCursor(0,0);
+    lcd.print("Tuning(cm): "+msg);
+    lcd.setCursor(0,1);
+    lcd.print("Height(cm): "+String(map(analogRead(Vol), 0, 4095, 0, 80)));
+  }
+  delay(400);
+  lcd.clear();
 }
 
-void IRAM_ATTR IO_INT_ISR()
+// Function detail
+void connect()
 {
-  /*
-    This method is called when lim_switch had pressed has 2 state
-      state 0 : command motor with speed tuing by PID
-      state 1 : command motor with 60% of speed
-  */
-  if (pre_btn != !digitalRead(LIM_SWITCH))
-  {
-    state = !digitalRead(LIM_SWITCH) ? 1 : 0;
-    pre_btn = !digitalRead(LIM_SWITCH);
+  // Connect Wifi screen
+  int i = 0;
+  if(WiFi.status() == WL_CONNECTED){
+    lcd.clear();
+    i = 0;
+    lcd.setCursor(1,0);
+    lcd.print("Connected WIFI");
+    delay(500);
+    lcd.clear();
   }
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    lcd.setCursor(1,0);
+    lcd.print("ConnectingWIFI");
+    lcd.setCursor(i,1);
+    lcd.print(".");
+    i++;
+    if(i>15){
+      i = 0;
+      lcd.clear();
+    }
+    delay(1000);
+  }
+  // Connect client
+  if(client.connect(mqtt_client_id)){
+    lcd.clear();
+    i = 0;
+    lcd.setCursor(0,0);
+    lcd.print("Connected Client");
+    delay(500);
+    lcd.clear();
+  }
+  while (!client.connect(mqtt_client_id))
+  {
+    lcd.setCursor(0,0);
+    lcd.print("ConnectingClient");
+    lcd.setCursor(i,1);
+    lcd.print(".");
+    i++;
+    if(i>15){
+      i = 0;
+      lcd.clear();
+    }
+    delay(1000);
+  }
+  // connect topic subscribe of mqtt
+  client.subscribe(mqtt_sub);
 }
 
 void messageReceived(String &topic, String &payload)
 {
-  /*
-    This method used to receive payload from mqtt server and assign values (setpoint) with payload has 2 state
-      state 0 : command motor with speed tuing by PID
-      state 1 : command motor with 60% of speed
-  */
-  Serial.println("incoming: " + topic + " - " + payload);
-  if (payload.toInt() > 0 && payload.toInt() <= 80)
-  {
-    setpoint = payload.toInt();
+  // get payload from heights to showing tuning stat from both of base box and server control(Node-red)
+  if(String(topic)=="project1.21/heights"){
+    if(pre_msg!=payload && payload.toInt()>=0){
+      pre_msg = payload;
+      msg = payload;
+      wait = true;
+    }
   }
-  else if (payload.toInt() == 0)
-  {
-    setpoint = 0;
-    state = 1;
+  // get payload from distance to alert completed tuning
+  if(String(topic)=="project1.21/distance"){
+    if(payload==msg && wait){
+      lcd.clear();
+      lcd.setCursor(6,1);
+      lcd.print("Done");
+      delay(1000);
+      wait = false;
+    }
   }
 }
 
-void connect()
+//// task for multicore function
+void task_arduino(void *arg)
 {
-  /* This method have 2 parts
-      part 1 (WIFI) : connect wifi
-      part 2 (MQTT) : connect mqtt and subscribe topic with variable (mqtt_height)
-  */
-  /* WIFI */
-  Serial.print("checking wifi...");
-  while (WiFi.status() != WL_CONNECTED)
+  while (1)
   {
-    Serial.print(".");
-    delay(1000);
+    if(digitalRead(Button_Pin) && !sent && !wait){
+      tone(Buzz, 500, 1200);
+      sent = true;
+      msg = String(map(analogRead(Vol), 0, 4095, 0, 80));
+    }
+    if(sent){
+      client.publish(mqtt_topic, msg);
+      sent = false;
+      wait = true;
+    }
   }
-
-  /* MQTT */
-  Serial.print("\nconnecting...");
-  while (!client.connect(mqtt_client_id))
-  {
-    Serial.print(".");
-    delay(1000);
-  }
-  Serial.println("\nconnected!");
-  client.subscribe(mqtt_height);
 }
